26edfd3b0e399d27119a76126a314a9ac4fda36f
Jeferson Lopes <jeferson.lopes@zup.com.br>
2019-01-29 14:32:26 -0200

    send local project

---
+++ app/src/main/java/com/example/jefersonlopes/backgroundservicedemo/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.jefersonlopes.backgroundservicedemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textResult = findViewById(R.id.textResult);
+    }
+
+    public void startBackgroundService(View view) {
+        //simple background service
+        Intent intent = new Intent(this, MyBackgroundService.class);
+        startService(intent);
+
+    }
+
+    public void stopBackgroundService(View view) {
+        //stop background service
+        Intent intent = new Intent(this, MyBackgroundService.class);
+        stopService(intent);
+
+    }
+
+    public void startIntentService(View view) {
+        //intent service is a best background service
+        Intent intent = new Intent(this, MyIntentService.class);
+        intent.putExtra("sleepTime", 12);
+        startService(intent);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //Using a broadcast receiver for get data of intent service
+        IntentFilter filter = new IntentFilter("my.own.broadcast");
+        LocalBroadcastManager.getInstance(this).registerReceiver(myLocalBroadcastReceiver, filter);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(myLocalBroadcastReceiver);
+    }
+
+    private BroadcastReceiver myLocalBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            int result = intent.getIntExtra("result",-1);
+            textResult.setText("Task is executed in: "+result+" seconds");
+
+        }
+    };
+}
---
+++ app/src/main/java/com/example/jefersonlopes/backgroundservicedemo/MyBackgroundService.java
@@ -0,0 +1,90 @@
+package com.example.jefersonlopes.backgroundservicedemo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyBackgroundService extends Service {
+
+    private static final String TAG = MyBackgroundService.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        Log.i(TAG, "onCreate, Thread name:"+Thread.currentThread().getName());
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "onStartCommand, Thread name:"+Thread.currentThread().getName());
+
+        //Perform tasks: Dummy long operation
+        new MyAsyncTask().execute(); //Background Thread
+
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.i(TAG, "onBind, Thread name:"+Thread.currentThread().getName());
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, "onDestroy, Thread name:"+Thread.currentThread().getName());
+        super.onDestroy();
+    }
+
+    class MyAsyncTask extends AsyncTask<Void, String, Void>{
+
+        @Override
+        protected void onPreExecute() {
+            Log.i(TAG, "onPreExecute, Thread name:"+Thread.currentThread().getName());
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Log.i(TAG, "doInBackground, Thread name:"+Thread.currentThread().getName());
+
+            //Dummy long operation
+            int ctr = 1;
+            while (ctr <= 12){
+
+                publishProgress("Time elapsed: "+ctr+" secs");
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                ctr++;
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+
+            Log.i(TAG, "onProgressUpdate, Counted value:"+values[0]+", Thread name:"+Thread.currentThread().getName());
+
+            Toast.makeText(MyBackgroundService.this, values[0], Toast.LENGTH_SHORT);
+
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Log.i(TAG, "onPostExecute, Thread name:"+Thread.currentThread().getName());
+            super.onPostExecute(aVoid);
+            stopSelf();
+        }
+    }
+
+}
